79c68e596142201c3ba1d985b2038fe5590ff34b
oscar <oscarfiend@gmail.com>
2021-08-12 15:10:22 -0500

    failed test case is updated

--- src/__tests__/videoList.test.js
+++ src/__tests__/videoList.test.js
@@ -3,26 +3,35 @@ import React from 'react';
 
 import VideoList from '../components/VideoList';
 import VideoContext from '../context/videoContext';
-import items from '../mocks/youtube-videos-mock.json'
 
 describe('VideoList component tests', () => {
-
   test('should containt a list container', () => {
-    render(<VideoContext><VideoList videos={items.items}/></VideoContext>);
+    render(
+      <VideoContext>
+        <VideoList />
+      </VideoContext>
+    );
     const list = screen.getByTestId(/container_videos/i);
     expect(list).toBeInTheDocument();
   });
 
-  test('should containt a card video list', () => {
-    render(<VideoContext><VideoList videos={items.items}/></VideoContext>);
+  test('should containt a card video list', async () => {
+    render(
+      <VideoContext>
+        <VideoList />
+      </VideoContext>
+    );
     const list = screen.findAllByTestId(/list_videos/i);
-    expect(list.length).toBe(25);
+    expect((await list).length).toBe(24);
   });
 
   test('shouldn´t containt a card video list', () => {
-    render(<VideoContext><VideoList videos={[]}/></VideoContext>);
+    render(
+      <VideoContext>
+        <VideoList />
+      </VideoContext>
+    );
     const list = screen.queryAllByTestId(/list_videos/i);
     expect(list.length).toBe(0);
   });
-
 });
