ca2b5f245bbd443a743a6e025063920d308b6555
oscar <oscarfiend@gmail.com>
2021-08-22 13:59:12 -0500

    lint is applied to the project

--- src/pages/NotFound/NotFound.page.jsx
+++ src/pages/NotFound/NotFound.page.jsx
@@ -6,10 +6,12 @@ import './NotFound.styles.css';
 function NotFoundPage() {
   return (
     <section className="not-found">
-      <Link to="/" className="home-link">
-        home
-      </Link>
-      <img src="404.gif" alt="page not found" />
+      <div className="not-found-background" />
+      <img className="not-found-image" src="error-logo-1.svg" alt="page not found" />
+      <strong className="not-found-description">
+        The page you were looking for doesn’t exist
+      </strong>
+      <Link to="/">Go Back</Link>
     </section>
   );
 }
